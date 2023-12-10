9567e7387ca16e92a1784255a7c855d15b2c8647
Sebastian C. Sabry <sebastiancsabry@system76-pc.localdomain>
2023-12-10 00:55:40 -0800

    The good old rusty chat bot

---
+++ src/app.rs
@@ -0,0 +1,21 @@
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+
+        // sets the document title
+        <Title text="Hello there, General Kenobi."/>
+
+        <ChatArea/>
+        <TypeArea/>
+    }
+}
