eda72bf46e1b0dea3579df40478ae2eeba15e768
Sebastian C. Sabry <sebastiancsabry@system76-pc.localdomain>
2023-12-11 01:04:04 -0800

    Creating conversations, messages struct, updated cargo tree structure

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use crate::model::conversation::Conversation;
+use crate::model::conversation::Message;
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
@@ -6,6 +8,19 @@ use leptos_router::*;
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
+
+    let (conversation, set_conversations) = create_signal(Conversation::new());
+
+    let send = create_action(move |new_message: &String| {
+        let user_message = Message {
+            text: new_message.clone(),
+            user: true,
+        };
+        set_conversations.update(move |c| {
+            c.messages.push(user_message);
+        });
+        // TODO
+    });
 
     view! {
         // injects a stylesheet into the document <head>
